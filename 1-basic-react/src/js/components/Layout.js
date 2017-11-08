0c24e85d25fed8cc3a2b74be75a553123030cac8
smithwebtek <brad@smithwebtek.com>
2017-11-08 14:34:46 -0500

    run npm install, webpack and verify app works

--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -7,7 +7,7 @@ export default class Layout extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: "Welcome",
+      title: "Welcome coders and civilians alike.",
     };
   }
 
